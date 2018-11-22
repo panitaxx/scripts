74c0accd2855e0149020cea9bbc20862ccfeab60
Iván Aponte <ivan.aponte@gmail.com>
2018-11-22 11:28:24 -0400

    add non interactive

--- deploy_compose.sh
+++ deploy_compose.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get update
-sudo apt-get install \
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -y remove docker docker-engine docker.io
+sudo apt-get -y  update
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -12,8 +12,8 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
